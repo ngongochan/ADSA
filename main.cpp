2c17e72191d08399139ead30b4a7cef491bfe5c6
Ngoc Han Ngo <161391723+ngongochan@users.noreply.github.com>
2025-08-23 10:23:34 +0000

    helper functions added

--- main.cpp
+++ main.cpp
@@ -4,18 +4,29 @@
 #include <algorithm>
 using namespace std;
 
+// helper function for converting from strings to int vectors
+vector<int> toVector(string s) {
+    vector<int> v;
+    transform(s.begin(), s.end(), back_inserter(v), [](char c) { return c - '0'; });
+    return v;
+}
+
+// helper function for converting from int vectors to strings
+string toString(vector<int> v) {
+    string s;
+    transform(v.begin(), v.end(), back_inserter(s), [](int i) { return i + '0'; });
+    return s;
+}
+
 // helper function for addition
 string intAddition(string a, string b, int base) {
-    vector<int> va;
-    vector<int> vb;
-    // converted from strings to int vectors correctly
-    transform(a.begin(), a.end(), back_inserter(va), [](char c) { return c - '0'; });
-    transform(b.begin(), b.end(), back_inserter(vb), [](char c) { return c - '0'; });
+    vector<int> va = toVector(a);
+    vector<int> vb = toVector(b);
     int digitA;
     int digitB;
-    vector<int> result;
     int carry = 0;
     int temp = 0;
+    vector<int> result;
     int lenA = va.size();
     int lenB = vb.size();
     while (lenA > 0 || lenB > 0 || carry == 1) {    // or digitA and digitB
@@ -35,13 +46,21 @@ string intAddition(string a, string b, int base) {
         lenB = vb.size();
     }
     std::reverse(result.begin(), result.end());
-    string s;
-    transform(result.begin(), result.end(), back_inserter(s), [](int i) { return i + '0'; });
+    string s = toString(result);
     return s;
 }
 
+// string intMultiplication(string a, string b, int base) {
+//     vector<int> va = toVector(a);
+//     vector<int> vb = toVector(b);
+
+//     string s = toString(result);
+//     return s;
+// }
+
 void calculate(string a, string b, int base) {
     string sum = intAddition(a, b, base);
+    // string sum = intMultiplication(a, b, base);
     std::cout << sum << " 0" << " 0" << std::endl;
 }
 
