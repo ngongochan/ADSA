2bc0ca62d0f04210358cd2a15477b4b5159b43a7
Ngoc Han Ngo <161391723+ngongochan@users.noreply.github.com>
2025-08-23 09:25:29 +0000

    right digits seem correct, still needs to work on multiplication

---
+++ convert.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+
+string convert(string s) {
+    // vector<int> v;
+    // int len = s.length();
+    // cout << len << endl;
+    // for (int i = 0; i < len; i++) {
+    //     cout << s[i];
+    // }
+
+    // // convert string to integer vector using transform
+    // transform(s.begin(), s.end(), back_inserter(v), [](char c) { return c - '0'; });
+    // for (int i = 0; i < len; i++) {
+    //     cout << v[i];
+    // }
+    return s;
+}
+
+int main() {
+    cout << convert("123456789");   // use this to print string to the terminal
+}
--- test.cpp
+++ test.cpp
@@ -6,10 +6,13 @@ using namespace std;
 string intAddition(string a, string b, int base) {
     vector<int> va;
     vector<int> vb;    
+    // convert string to integer vector using transform
     transform(a.begin(), a.end(), back_inserter(va), [](char c) { return c - '0'; });
     transform(b.begin(), b.end(), back_inserter(vb), [](char c) { return c - '0'; });
+
     int digitA;
     int digitB;
+    // int multiplier = 1;
     vector<int> result;
     int carry = 0;
     int temp = 0;
@@ -26,25 +29,35 @@ string intAddition(string a, string b, int base) {
             carry = 0;            
         }
         result.push_back(temp);
+        // multiplier *= 10;
         va.pop_back();
         vb.pop_back();
         lenA = va.size();
         lenB = vb.size();        
     }
+    // reverse vector result
     std::reverse(result.begin(), result.end());
+
+    // convert int vector to string
     string s;
     transform(result.begin(), result.end(), back_inserter(s), [](int i) { return i + '0'; });
+
+    // return string result
     return s;
 }
 
+/*
 void calculate(string a, string b, int base) {
-    string sum = intAddition(a, b, base);
-    std::cout << sum << " 0" << " 0" << std::endl;
+    // int sum = intAddition(a, b, base);
+    // std::cout << sum << " 0" << " 0" << std::endl;
 }
+*/
 
 int main() {
     string a, b;
     int base;
     std::cin >> a >> b >> base;
-    calculate(a, b, base);
+    // this works but with a condition of input numbers being strings
+    // cout << intAddition("456", "123", 7) << endl;
+    cout << intAddition("33774022283743315542623650011778280736723644410316427185", "36823874817726422221662722", 9) << endl;
 }
