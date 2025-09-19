82ba935a54322ebf15f1402be16781314cb6dd5b
Ngoc Han Ngo <161391723+ngongochan@users.noreply.github.com>
2025-09-19 13:16:57 +0000

    search and traverse done

---
+++ Practical-2/design.py
@@ -0,0 +1,23 @@
+# values are distinct and not repeated
+    # ask for user input aka modification moves, then store moves into a list of strings
+        # the last element of that list will indicate which order the AVL tree should be printed in
+        # for other moves, e.g., A3, D4
+            # the letter "A" and "D" are short for "Add" and "Delete"
+            # follow each letter is the value of the node to be added or deleted
+                # e.g., A3 means to add 3 to the tree
+
+    # helper function to add value
+
+    # helper function to delete value
+        # delete a leaf
+        # delete a node with 1 child
+        # delete a node with 2 children
+
+    # HELPER FUNCTION TO TURN AN ORDINARY BST INTO AN AVL TREE
+
+    # helper functions to traverse the entire tree and print out all values - DONE
+        # inorder
+        # preorder
+        # postorder
+
+    # hmm maybe make one traverse function only - DONE
---
+++ Practical-2/main.py
@@ -0,0 +1,75 @@
+class Node:
+    def __init__(self, value = 0, left = None, right = None):
+        self.value = value
+        self.right = right
+        self.left = left
+
+class AVLTree:
+    def __init__(self, root = None):
+        self.root = root
+
+    def search(self, target, node=None):
+        if node == None:
+            node = self.root
+        if(node == None or node.value == target):
+            return node
+        if(target > node.value):
+            return self.search(target, node.right)
+        else:
+            return self.search(target, node.left)
+
+    # def search(self, root, target):
+    #     if(root.value == target or root == None):
+    #         return root
+    #     if(target > root.value):
+    #         return self.search(root.right, target)
+    #     else:
+    #         return self.search(root.left, target)
+
+    def traverse(self, order):
+        if order == "PRE":
+            self.preorder(self.root)
+        elif order == "IN":
+            self.inorder(self.root)
+        elif order == "POST":
+            self.postorder(self.root)
+        else:
+            print("invalid")
+
+    def inorder(self, node):
+        if node == None:
+            return
+        self.inorder(node.left)
+        print(node.value)
+        self.inorder(node.right)
+
+    def preorder(self, node):
+        if node == None:
+            return
+        print(node.value)
+        self.preorder(node.left)
+        self.preorder(node.right)
+        
+    def postorder(self, node):
+        if node == None:
+            return
+        self.postorder(node.left)
+        self.postorder(node.right)
+        print(node.value)
+        # works the same way
+        # if root != None:
+        #     self.postorder(root.left)
+        #     self.postorder(root.right)
+        #     print(root.value)
+
+
+#        5
+# 1             10
+root = Node(5, Node(1), Node(10))
+avl = AVLTree(root)
+# avl.postorder(root)   # avl.traverse("POST")
+node = avl.search(1)
+if node:
+    print(node.value)
+else:
+    print("not found")
