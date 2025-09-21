014f7b719f6c4ab0abbdf337ce28e64d19f9b5b6
Ngoc Han Ngo <161391723+ngongochan@users.noreply.github.com>
2025-09-21 13:27:16 +0000

    test

--- main.py
+++ main.py
@@ -39,8 +39,6 @@ class AVLTree:
             return self.left_rotate(node)
         return node
 
-
-
     def right_rotate(self, y):
         x = y.left
         T2 = x.right
@@ -49,6 +47,15 @@ class AVLTree:
         y.height = 1 + max(self.get_height(y.left), self.get_height(y.right))
         x.height = 1 + max(self.get_height(x.left), self.get_height(x.right))
         return x
+
+    def left_rotate(self, x):
+        y = x.right
+        T2 = y.left
+        y.left = x
+        x.right = T2
+        x.height = 1 + max(self.get_height(x.left), self.get_height(x.right))
+        y.height = 1 + max(self.get_height(y.left), self.get_height(y.right))
+        return y
 
     def insert(self, node, value):
         if not node:
@@ -59,23 +66,7 @@ class AVLTree:
         else:
             node.right = self.insert(node.right, value)
         node.height = 1 + max(self.get_height(node.left), self.get_height(node.right))
-        balance = self.get_balance(node)
-
-        # LL
-        if balance > 1 and value < node.left.value:
-            return self.right_rotate(node)
-        # RR
-        if balance < -1 and value > node.right.value:
-            return self.left_rotate(node)
-        # LR
-        if balance > 1 and value > node.left.value:
-            node.left = self.left_rotate(node.left)
-            return self.right_rotate(node)
-        # RL
-        if balance < -1 and value < node.right.value:
-            node.right = self.right_rotate(node.right)
-            return self.left_rotate(node)
-        return node
+        return self.rebalance(node)
 
     def insert_value(self, value):
         self.root = self.insert(self.root, value)
@@ -108,11 +99,8 @@ class AVLTree:
         node.height = 1 + max(self.get_height(node.left), self.get_height(node.right))
         return self.rebalance(node)
 
-
-
-
-
-
+    def delete_value(self, value):
+        self.root = self.delete(self.root, value)
 
     def traverse(self, order):
         if order == "PRE":
@@ -154,9 +142,10 @@ if __name__ == "__main__":
     # print(moves)
     for move in moves:
         if move[0] == "A":
-            value = move[1:]
+            value = int(move[1:])
             avl.insert_value(value)
         elif move[0] == "D":
-            pass
+            value = int(move[1:])
+            avl.delete_value(value)
         else:
-            avl.traverse(move)
+            print(avl.traverse(move))
