6b1f8dc4039647b14c33654ed0153388f7e8879d
Ngoc Han Ngo <161391723+ngongochan@users.noreply.github.com>
2025-10-18 19:10:27 +0000

    seems to work

---
+++ main.py
@@ -0,0 +1,81 @@
+class Slot:
+    def __init__(self):
+        self.value = None
+        self.status = "never used"
+
+class HashTable:
+    def __init__(self):
+        self.slots = [Slot() for _ in range(26)]
+
+    def search(self, word):
+        start_index = ord(word[-1]) - ord('a')
+        current_index = start_index
+
+        while True:
+            slot = self.slots[current_index]
+            if slot.value == word:
+                return True
+            if slot.status == "never used":
+                return False
+
+            current_index = (current_index + 1) % 26
+            if current_index == start_index:
+                return False
+
+    def insert(self, word):
+        if self.search(word):
+            return
+
+        index = ord(word[-1]) - ord('a')
+        start_index = index
+
+        while True:
+            slot = self.slots[index]
+            if slot.status in ("tombstone", "never used"):
+                slot.value = word
+                slot.status = "occupied"
+                return
+
+            index = (index + 1) % 26
+            if index == start_index:
+                return
+
+    def delete(self, word):
+        if not self.search(word):
+            return  # not found, nothing to do
+
+        index = ord(word[-1]) - ord("a")
+        start_index = index
+
+        while True:
+            slot = self.slots[index]
+
+            if slot.value == word:
+                slot.value = None
+                slot.status = "tombstone"
+                return
+
+            index = (index + 1) % 26
+            if index == start_index:
+                return
+                
+    def traverse(self):
+        for i, slot in enumerate(self.slots):
+            if (slot.status == "occupied"):
+                print(slot.value, end=" ")
+
+
+if __name__ == "__main__":
+    ht = HashTable()
+    moves = input("Enter moves: ").split()
+
+    for move in moves:
+        action = move[0]
+        word = move[1:]
+
+        if action == "A":
+            ht.insert(word)
+        else:
+            ht.delete(word)
+
+    ht.traverse()
