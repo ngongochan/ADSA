a2925fe1e935082f57cd32dc18458d727474b3a6
Ngoc Han Ngo <161391723+ngongochan@users.noreply.github.com>
2025-11-02 13:07:16 +0000

    second try with different approach, please work

--- main.py
+++ main.py
@@ -1,76 +1,78 @@
-class UnionFind:
-    def __init__(self, n):
-        self.parent = list(range(n))
-        self.rank = [0] * n
-    
-    def find(self, x):
-        if self.parent[x] != x:
-            self.parent[x] = self.find(self.parent[x])
-        return self.parent[x]
-    
-    def union(self, x, y):
-        rootX = self.find(x)
-        rootY = self.find(y)
-        
-        if rootX != rootY:
-            if self.rank[rootX] > self.rank[rootY]:
-                self.parent[rootY] = rootX
-            elif self.rank[rootX] < self.rank[rootY]:
-                self.parent[rootX] = rootY
-            else:
-                self.parent[rootY] = rootX
-                self.rank[rootX] += 1
-            return True
-        return False
+class CountrySystem():
+    def __init__(self, data):
+        country_data, build_data, destroy_data = data.split(" ")
+        country_rows = country_data.split(",")
+        build_rows = build_data.split(",")
+        destroy_rows = destroy_data.split(",")
+        total_countries = len(country_rows)
+        self.length = total_countries
+        self.country = [[0 for _ in range(total_countries)] for _ in range(total_countries)]
+        self.build = [[None for _ in range(total_countries)] for _ in range(total_countries)]
+        self.destroy = [[None for _ in range(total_countries)] for _ in range(total_countries)]
+        for row_index in range(total_countries):
+            for col_index in range(total_countries):
+                self.country[row_index][col_index] = int(country_rows[row_index][col_index])
+                self.build[row_index][col_index] = self.interpretCost(build_rows[row_index][col_index])
+                self.destroy[row_index][col_index] = self.interpretCost(destroy_rows[row_index][col_index])
 
-def letter_to_cost(letter):
-    """converts a letter (A-Z, a-z) to its respective cost value"""
-    if 'A' <= letter <= 'Z':
-        return ord(letter) - ord('A')
-    return ord(letter) - ord('a') + 26
+    def interpretCost(self, digit):
+        ascii_value = ord(digit)
+        if ord('a') <= ascii_value <= ord('z'):
+            return ascii_value - ord('a') + 26
+        if ord('A') <= ascii_value <= ord('Z'):
+            return ascii_value - ord('A')
+        return -1
 
-def solve(n, country, build, destroy):
-    edges = []
-    
-    for i in range(n):
-        for j in range(i + 1, n):
-            if country[i][j] == '1':
-                destroy_cost = letter_to_cost(destroy[i][j])
-                edges.append((destroy_cost, i, j, 'destroy'))
+    def optimizeCost(self):
+        parent = [country_id for country_id in range(self.length)]
+        rank = [0 for _ in range(self.length)]
+
+        def find(country_id):
+            return country_id if parent[country_id] == country_id else find(parent[country_id])
+
+        def union(country_a, country_b):
+            root_a = find(country_a)
+            root_b = find(country_b)
+            if root_a == root_b:
+                return False
             else:
-                build_cost = letter_to_cost(build[i][j])
-                edges.append((build_cost, i, j, 'build'))
-    
-    edges.sort()
-    
-    uf = UnionFind(n)
-    total_cost = 0
-    for cost, u, v, _ in edges:
-        if uf.union(u, v):  # If u and v are not in the same component, connect them
-            total_cost += cost
-    
-    return total_cost
+                if rank[root_a] >= rank[root_b]:
+                    rank[root_a] += 1
+                    parent[root_b] = root_a
+                else:
+                    parent[root_a] = root_b
+                return True
+
+        def DSU(edge_pairs):
+            mst_cost = 0
+            for edge_weight, start_country, end_country in edge_pairs:
+                if union(start_country, end_country):
+                    mst_cost += edge_weight
+            return mst_cost
+
+        edge_pairs = []
+        total_destroy_cost = 0
+        for start_country in range(self.length):
+            for end_country in range(start_country + 1, self.length):
+                if self.country[start_country][end_country] == 1:
+                    total_destroy_cost += self.destroy[start_country][end_country]
+                    edge_weight = -self.destroy[start_country][end_country]
+                else:
+                    edge_weight = self.build[start_country][end_country]
+                edge_pairs.append((edge_weight, start_country, end_country))
+
+        edge_pairs.sort(key=lambda edge: edge[0])
+        mst_cost = DSU(edge_pairs)
+        return mst_cost + total_destroy_cost
+
 
 def main():
-    country = [
-        '000',
-        '000',
-        '000'
-    ]
-    build = [
-        'ABD',
-        'BAC',
-        'DCA'
-    ]
-    destroy = [
-        'ABD',
-        'BAC',
-        'DCA'
-    ]
-    n = 3
-    
-    result = solve(n, country, build, destroy)
-    print(result)
+    data = input()
+    country_system = CountrySystem(data)
+    cost = country_system.optimizeCost()
+    print(cost)
+    return
+
 
 if __name__ == "__main__":
     main()
